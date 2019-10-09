545c39c4ba24d78562e50a022f37a0f0b5693894
Mukhlis J <mukhlis.jamato@gmail.com>
2019-10-09 15:17:16 +0800

    Updated table css
    updated table styling and contents. revised icons for features

--- js/new-age.js
+++ js/new-age.js
@@ -15,6 +15,11 @@
     }
   });
 
+  // Stop carousel
+  $('.carousel').carousel({
+        interval: false
+  });
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
@@ -40,3 +45,5 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+
